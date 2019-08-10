406c50a995a08c7a275e8fae08ac61d141f2cce8
shramko.serhii <shramko.web@yahoo.com>
2019-08-10 13:18:10 +0300

    feat: add show-more button module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {getSearch} from "./components/search";
 import {getProfile} from "./components/profile";
 import {getNavigation} from "./components/navigation";
 import {getFilmCard} from "./components/film-card";
+import {getShowMoreButton} from "./components/show-more-button";
 
 /* Ф-я рендера компонента */
 const renderComponent = (container, component) => {
