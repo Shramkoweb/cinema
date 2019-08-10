d93a750baf999e5917ff6f0ac605f67564bbfea4
shramko.serhii <shramko.web@yahoo.com>
2019-08-10 14:27:30 +0300

    feat: add films module

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import {getSearch} from "./components/search";
 import {getProfile} from "./components/profile";
 import {getNavigation} from "./components/navigation";
-import {getFilmCard} from "./components/film-card";
-import {getShowMoreButton} from "./components/show-more-button";
 import {getFilmDetails} from "./components/film-details";
+import {getFilms} from "./components/films";
+
+const headerElement = document.querySelector(`header`);
+const mainElement = document.querySelector(`.main`);
+const bodyElement = document.querySelector(`body`);
 
 /* Ф-я рендера компонента */
 const renderComponent = (container, component) => {
   return container.insertAdjacentHTML(`beforeend`, component);
 };
+
+renderComponent(headerElement, getSearch());
+renderComponent(headerElement, getProfile());
+renderComponent(mainElement, getNavigation());
+renderComponent(mainElement, getFilms(5));
+renderComponent(bodyElement, getFilmDetails());
