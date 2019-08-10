c79aa6d0d0f6f648624a9beb8bfc1077348f80c5
shramko.serhii <shramko.web@yahoo.com>
2019-08-10 13:12:27 +0300

    feat: add navigation module

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {getSearch} from "./components/search";
 import {getProfile} from "./components/profile";
+import {getNavigation} from "./components/navigation";
 
 /* Ф-я рендера компонента */
 const renderComponent = (container, component) => {
