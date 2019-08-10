d191676a7c36cde21f91369a73fd334c3636b7e8
shramko.serhii <shramko.web@yahoo.com>
2019-08-10 13:15:48 +0300

    feat: add film card module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {getSearch} from "./components/search";
 import {getProfile} from "./components/profile";
 import {getNavigation} from "./components/navigation";
+import {getFilmCard} from "./components/film-card";
 
 /* Ф-я рендера компонента */
 const renderComponent = (container, component) => {
