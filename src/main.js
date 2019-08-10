3a0eb8bba16f2dea1980c4fbfffec2d371ca359f
shramko.serhii <shramko.web@yahoo.com>
2019-08-10 13:23:15 +0300

    feat: add film-details module

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {getProfile} from "./components/profile";
 import {getNavigation} from "./components/navigation";
 import {getFilmCard} from "./components/film-card";
 import {getShowMoreButton} from "./components/show-more-button";
+import {getFilmDetails} from "./components/film-details";
 
 /* Ф-я рендера компонента */
 const renderComponent = (container, component) => {
