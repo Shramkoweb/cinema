e88b3e583ad8f03b357edc28a5f73a5c6989c2ff
Serhii.Shramko <shramko.web@yahoo.com>
2019-08-26 17:51:33 +0300

    refactor: add Profile class

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,32 +1,52 @@
 /* Получаем рейтинг пользователя */
-const getUserRating = (movies) => {
-  let watchedMovies = 0;
-  movies.forEach((movie) => {
-    if (movie.isWatched) {
-      watchedMovies++;
+import {createElement} from "../util";
+
+export default class Profile {
+  constructor(movies) {
+    this._element = null;
+    this._movies = movies;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
     }
-  });
 
-  const getUserTitle = (moviesWatched) => {
-    let userTitle = ``;
-    if (moviesWatched >= 21) {
-      userTitle = `Movie Buff`;
-    } else if (moviesWatched >= 11) {
-      userTitle = `fan`;
-    } else if (moviesWatched > 0) {
-      userTitle = `novice`;
-    }
-    return userTitle;
-  };
+    return this._element;
+  }
 
-  return getUserTitle(watchedMovies);
-};
+  removeElement() {
+    this._element = null;
+  }
 
-export const getProfile = (movies) => {
-  return `   
-    <section class="header__profile profile">
-      <p class="profile__rating">${getUserRating(movies)}</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    </section>
-  `.trim();
-};
+  getTemplate() {
+    const getUserRating = (movies) => {
+      let watchedMovies = 0;
+      movies.forEach((movie) => {
+        if (movie.isWatched) {
+          watchedMovies++;
+        }
+      });
+
+      const getUserTitle = (moviesWatched) => {
+        let userTitle = ``;
+        if (moviesWatched >= 21) {
+          userTitle = `Movie Buff`;
+        } else if (moviesWatched >= 11) {
+          userTitle = `fan`;
+        } else if (moviesWatched > 0) {
+          userTitle = `novice`;
+        }
+        return userTitle;
+      };
+
+      return getUserTitle(watchedMovies);
+    };
+    return `
+      <section class="header__profile profile">
+        <p class="profile__rating">${getUserRating(this._movies)}</p>
+        <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      </section>
+    `.trim();
+  }
+}
