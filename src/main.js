25cdacd0f08c093f2a9e3abc18a6eda53e6eaab9
Serhii.Shramko <shramko.web@yahoo.com>
2019-08-26 22:01:16 +0300

    refactor: implement new realisation of dynamic load movies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Search from "./components/search";
 import Navigation from "./components/navigation";
 import MovieDetails from "./components/movie-details";
 import {getFilterCount} from "./filter";
-import {getRandomNumberInRange, isEscKeyDown, Position, renderElement} from "./util";
+import {getRandomNumberInRange, isEscKeyDown, Position, renderElement, unrenderElement} from "./util";
 import {getMovies} from "./data";
 import Profile from "./components/profile";
 import Movie from "./components/movie";
@@ -14,39 +14,6 @@ const MAX_MOVIES_TO_RENDER = 5;
 
 const headerElement = document.querySelector(`header`);
 const mainElement = document.querySelector(`.main`);
-const bodyElement = document.querySelector(`body`);
-
-//
-// renderComponent(headerElement, getSearch());
-// renderComponent(headerElement, getProfile(MOVIES));
-// renderComponent(mainElement, getNavigationTemplate(getFilterCount(MOVIES)));
-// renderComponent(mainElement, getBoardTemplate(MOVIES.slice(0, MAX_MOVIES_TO_RENDER)));
-// renderComponent(bodyElement, getMovieDetailsTemplate(MOVIES[0]));
-//
-//
-// const loadMoreButton = mainElement.querySelector(`.films-list__show-more`);
-// const filmListElement = mainElement.querySelector(`.films-list__container`);
-//
-// let MOVIES_ON_PAGE = MAX_MOVIES_TO_RENDER;
-// let LEFT_MOVIES_TO_RENDER = MOVIES.length - MOVIES_ON_PAGE;
-//
-// const renderLeftCards = () => {
-//   renderComponent(filmListElement, getMoviesTamplate(MOVIES.slice(MOVIES_ON_PAGE, (MOVIES_ON_PAGE + MAX_MOVIES_TO_RENDER))));
-//
-//   MOVIES_ON_PAGE = MOVIES_ON_PAGE + MAX_MOVIES_TO_RENDER;
-//   LEFT_MOVIES_TO_RENDER = MOVIES.length - MOVIES_ON_PAGE;
-//
-//   if (LEFT_MOVIES_TO_RENDER <= 0) {
-//     loadMoreButton.classList.add(`visually-hidden`);
-//     loadMoreButton.removeEventListener(`click`, onLoadMoreButtonClick);
-//   }
-// };
-//
-// const onLoadMoreButtonClick = () => {
-//   renderLeftCards();
-// };
-//
-// loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
 
 const board = new Movies().getElement();
 const moviesContainer = board.querySelector(`.films-list .films-list__container`);
@@ -70,11 +37,6 @@ const renderNavigation = (filters) => {
 
   renderElement(mainElement, navigationInstance.getElement(), Position.BEFOREEND);
 };
-
-renderSearch();
-renderProfile(MOVIES);
-renderNavigation(getFilterCount(MOVIES));
-
 
 const renderMovies = (movies) => {
   const fragment = document.createDocumentFragment();
@@ -109,4 +71,33 @@ const renderBoard = (movies) => {
   renderElement(mainElement, board, Position.BEFOREEND);
 };
 
-renderBoard(MOVIES);
+renderSearch();
+renderProfile(MOVIES);
+renderNavigation(getFilterCount(MOVIES));
+renderBoard(MOVIES.slice(0, MAX_MOVIES_TO_RENDER));
+
+const loadMoreButton = mainElement.querySelector(`.films-list__show-more`);
+
+
+let MOVIES_ON_PAGE = MAX_MOVIES_TO_RENDER;
+let LEFT_MOVIES_TO_RENDER = MOVIES.length - MOVIES_ON_PAGE;
+
+const renderLeftMovies = () => {
+  moviesContainer.appendChild(renderMovies(MOVIES.slice(MOVIES_ON_PAGE, (MOVIES_ON_PAGE + MAX_MOVIES_TO_RENDER))));
+
+  MOVIES_ON_PAGE = MOVIES_ON_PAGE + MAX_MOVIES_TO_RENDER;
+  LEFT_MOVIES_TO_RENDER = MOVIES.length - MOVIES_ON_PAGE;
+
+  if (LEFT_MOVIES_TO_RENDER <= 0) {
+    unrenderElement(loadMoreButton);
+  }
+};
+
+const onLoadMoreButtonClick = () => {
+  renderLeftMovies();
+};
+
+loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
+
+
+console.table(MOVIES);
