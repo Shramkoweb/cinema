e35406f849e92df208925fa8f2f41b28385354a7
Serhii.Shramko <shramko.web@yahoo.com>
2019-09-01 12:53:27 +0300

    refactor: make some changes in Movie class - extend Movie from AbstractComponent - add movieYear getter - add addActiveClass method

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,8 +1,8 @@
-import {createElement} from "../util";
+import AbstractComponent from "./absctract-component";
 
-export default class Movie {
+export default class Movie extends AbstractComponent {
   constructor({title, rating, releaseDate, runtime, genres, image, description, comments, isFavorite, isWatched, isInWatchlist}) {
-    this._element = null;
+    super();
     this._title = title;
     this._rating = rating;
     this._releaseDate = releaseDate;
@@ -16,35 +16,13 @@ export default class Movie {
     this._isInWatchlist = isInWatchlist;
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
-
   getTemplate() {
-    /* Получаем год из даты */
-    const getMovieYear = (date) => {
-      return new Date(date).getFullYear();
-    };
-
-    /* Возврат активного класса для елемента */
-    const elementAddActiveClass = (isActive) => {
-      return `${isActive ? `active` : ``}`;
-    };
-
     return `
       <article class="film-card">
         <h3 class="film-card__title">${this._title}</h3>
         <p class="film-card__rating">${this._rating}</p>
         <p class="film-card__info">
-          <span class="film-card__year">${getMovieYear(this._releaseDate)}</span>
+          <span class="film-card__year">${this.movieYear}</span>
           <span class="film-card__duration">${Movie.getMovieDuration(this._runtime)}</span>
           <span class="film-card__genre">${[...this._genres][0]}</span>
         </p>
@@ -52,12 +30,22 @@ export default class Movie {
         <p class="film-card__description">${this._description}</p>
         <a class="film-card__comments">${this._comments.length} comments</a>
         <form class="film-card__controls">
-          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--${elementAddActiveClass(this._isInWatchlist)}">Add to watchlist</button>
-          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--${elementAddActiveClass(this._isWatched)}">Mark as watched</button>
-          <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--${elementAddActiveClass(this._isFavorite)}">Mark as favorite</button>
+          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--${this.addActiveClass(this._isInWatchlist)}">Add to watchlist</button>
+          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--${this.addActiveClass(this._isWatched)}">Mark as watched</button>
+          <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--${this.addActiveClass(this._isFavorite)}">Mark as favorite</button>
         </form>
       </article>
     `.trim();
+  }
+
+  /* Получаем год из даты */
+  get movieYear() {
+    return new Date(this._releaseDate).getFullYear();
+  }
+
+  /* Возврат активного класса для карточек */
+  addActiveClass(isActive) {
+    return `${isActive ? `active` : ``}`;
   }
 
   static getMovieDuration(duration) {
