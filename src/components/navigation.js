0bf2cae3bb1919054c732ad59cef0ded6be4fdab
Serhii.Shramko <shramko.web@yahoo.com>
2019-09-01 14:12:13 +0300

    refactor: extend Navigation from AbstractComponent

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,40 +1,28 @@
-import {createElement} from "../util";
+import AbstractComponent from "./absctract-component";
 
-export default class Navigation {
+export default class Navigation extends AbstractComponent {
   constructor(filters) {
-    this._element = null;
+    super();
     this._filters = filters;
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
-
-  getTemplate() {
-    const getFilterTemplate = ({title, count}) => {
-      return `
+  getFilterTemplate({title, count}) {
+    return `
         <a href="#${title}" class="main-navigation__item">
           ${title} <span class="main-navigation__item-count">${count}</span>
         </a>
       `.trim();
-    };
+  }
 
-    const getFiltersTemplate = (filters) => {
-      return filters.map((filter) => getFilterTemplate(filter)).join(``);
-    };
+  getFiltersTemplate(filters) {
+    return filters.map((filter) => this.getFilterTemplate(filter)).join(``);
+  }
 
+  getTemplate() {
     return `
       <nav class="main-navigation">
         <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        ${getFiltersTemplate(this._filters)}
+        ${this.getFiltersTemplate(this._filters)}
       </nav>
     `.trim();
   }
