b72918c5c621784bb15bb2ef43b870b3323d2e02
Serhii.Shramko <shramko.web@yahoo.com>
2019-09-01 15:21:38 +0300

    refactor: remove unused export in films class

--- src/components/films.js
+++ src/components/films.js
@@ -1,10 +1,8 @@
 import AbstractComponent from "./absctract-component";
-import ShowMoreButton from "./show-more-button";
 
 export default class Films extends AbstractComponent {
   constructor() {
     super();
-    this._moreButtonTemplate = new ShowMoreButton().getTemplate();
   }
 
   getTemplate() {
