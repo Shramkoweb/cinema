629ac4b7049100f396c858f55688d2e757abc70a
Serhii.Shramko <shramko.web@yahoo.com>
2019-09-02 08:25:22 +0300

    fix: fix render films on more button click

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -8,6 +8,7 @@ import Profile from "../components/profile";
 import {getLastTwoSortedItemsFrom, isEscKeyDown, Position, renderElement, sortByComments, sortByRating, unrenderElement} from "../util";
 
 const MAX_FILMS_TO_RENDER = 5;
+let FILMS_ON_PAGE = 5;
 
 export default class PageController {
   constructor(container, filmCards) {
@@ -64,8 +65,6 @@ export default class PageController {
   }
 
   _renderLeftFilms(films) {
-    let FILMS_ON_PAGE = 5;
-
     films
       .slice(FILMS_ON_PAGE, (FILMS_ON_PAGE + MAX_FILMS_TO_RENDER))
       .forEach((film) => this._renderFilms(film, this._filmsContainer));
@@ -84,7 +83,7 @@ export default class PageController {
     const footerStatisticsElement = document.querySelector(`.footer__statistics p`);
     footerStatisticsElement.textContent = `${this._filmCards.length} movies inside`;
 
-    // if 0 films
+    // 0 films
     if (!this._hasFilms) {
       renderElement(this._container, this._films.getElement(), Position.BEFOREEND);
 
