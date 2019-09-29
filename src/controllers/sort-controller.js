2b021a45d18c0a4ec70e8d0bba6bbb466ae7902e
Serhii.Shramko <shramko.web@yahoo.com>
2019-09-29 20:58:28 +0300

    chore: add sort-controller

--- src/controllers/sort-controller.js
+++ src/controllers/sort-controller.js
@@ -3,10 +3,26 @@ import {renderElement, sortFilms} from "../utils";
 import {Position} from "../constants";
 
 class SortController {
-  constructor(container) {
+  constructor(container, renderFilmsList) {
     this._container = container;
+    this._renderFilmsList = renderFilmsList;
     this._sortComponent = new Sort();
   }
+
+  show() {
+    this._sortComponent.getElement().classList.remove(`visually-hidden`);
+  }
+
+  hide() {
+    this._sortComponent.getElement().classList.add(`visually-hidden`);
+  }
+
+
+  assignFilms(films) {
+    this._films = films;
+    this._sorteredFilms = films.slice();
+  }
+
 
   init() {
     const onSortLinkClick = (evt) => {
@@ -17,34 +33,27 @@ class SortController {
         .querySelector(`.sort__button--active`).classList
         .remove(`sort__button--active`);
 
-      // Активный класс на целевой елемент
       evt.target.classList.add(`sort__button--active`);
 
-      const sortType = evt.target.dataset.sortType;
+      if (evt.target.tagName === `A`) {
+        // Активный класс на целевой елемент
+        evt.target.classList.add(`sort__button--active`);
 
-      this._films = sortFilms(this._films, sortType);
-      console.log(this._films);
+
+        // Тип Сортировки в зависимости от нажатой кнопки
+        const sortType = evt.target.dataset.sortType;
+
+
+        // отрисовка фильмов
+        this._sorteredFilms = sortFilms(this._films, sortType);
+        this._renderFilmsList(this._sorteredFilms);
+      }
     };
 
     // Вешаю обработчики клика на кнопки сортировки
-    this._sortComponent.getElement().querySelectorAll(`.sort__button`).forEach((elemBtnSort) => {
-      elemBtnSort.addEventListener(`click`, onSortLinkClick);
-    });
+    this._sortComponent.getElement().addEventListener(`click`, (evt) => onSortLinkClick(evt));
 
     renderElement(this._container, this._sortComponent.getElement(), Position.AFTERBEGIN);
-  }
-
-  assignFilms(films) {
-    this._films = films;
-    this._sorteredFilms = films.slice();
-  }
-
-  show() {
-    this._sortComponent.getElement().classList.remove(`visually-hidden`);
-  }
-
-  hide() {
-    this._sortComponent.getElement().classList.add(`visually-hidden`);
   }
 }
 
