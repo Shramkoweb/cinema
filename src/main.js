6bddbce05dd47563913b480c09aa96199f9368d1
Serhii.Shramko <shramko.web@yahoo.com>
2019-10-04 20:10:33 +0300

    chore: updates main

--- src/main.js
+++ src/main.js
@@ -1,67 +1,155 @@
-import PageController from "./controllers/page-controller";
-import Statistics from "./components/statistics";
 import API from "./api";
-import StatisticController from "./controllers/statistic-controller";
+import Loading from "./components/loading";
+import MenuController from "./controllers/menu-controller";
+import PageController from "./controllers/page-controller";
+import Profile from "./components/profile";
+import Search from "./components/search";
 import SearchController from "./controllers/search-controller";
+import StatisticController from "./controllers/statistic-controller";
+import {AUTHORIZATION, MIN_SEARCH_PHRASE, URL, ActionType} from "./constants";
+import {renderElement, unrenderElement} from "./utils";
 
-const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=${Math.random()}`;
-const END_POINT = `https://htmlacademy-es-9.appspot.com/cinemaddict/`;
-
+const footerFilmsAmountElement = document.querySelector(`.footer__statistics p`);
+const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 
-const onDataChange = (actionType, updatedFilm) => {
-  switch (actionType) {
-    case `update`:
-      api.updateMovie({
-        id: updatedFilm.id,
-        data: updatedFilm.toRAW(),
-      })
-        .then(() => api.getMovies())
-        .then((movies) => {
-          if (filmsControllerInstance._isSearchActive) {
-            const searchController = new SearchController(filmsControllerInstance._phrase, movies);
+// API & статические компоненты
+const api = new API({authorization: AUTHORIZATION, endPoint: URL});
+const searchComponent = new Search();
+const loadingComponent = new Loading();
 
-            const sortedFilms = searchController.search();
-            filmsControllerInstance._renderFilms(sortedFilms, filmsControllerInstance._filmsContainerElement);
-          } else {
-            filmsControllerInstance._renderUpdatesFilms();
-          }
+
+// Изменения состояния котроллеров при активации поиска
+const setSearchState = (state) => {
+  menuController.setSearch(state);
+  pageController.setSearch(state);
+  searchController.setSearch(state);
+};
+
+const onSearchCloseClick = () => {
+  searchController.hide();
+  chartController.hide();
+
+  // Сбрасиваем все контроллеры
+  setSearchState(false);
+
+  // отрисовка страницы п исходное состояние
+  onDataChange(ActionType.CREATE);
+};
+
+
+const onDataChange = (actionType, updatedFilm, callBackFunction) => {
+  switch (actionType) {
+    case ActionType.UPDATE:
+      api.updateFilm({
+        id: updatedFilm.id,
+        film: updatedFilm.toRAW(),
+      })
+        .then(() => api.getFilms())
+        .then((movies) => {
+          menuController.show(movies);
+          pageController.show(movies);
+          searchController.show(movies);
         });
       break;
+    case ActionType.CREATE:
+      renderElement(mainElement, loadingComponent.getElement());
+      api.getFilms().then((movies) => {
+        unrenderElement(loadingComponent.getElement());
+        loadingComponent.removeElement();
+        menuController.show(movies);
+        pageController.show(movies);
+        searchController.show(movies);
+        footerFilmsAmountElement.textContent = `${movies.length} movies inside`;
+      });
+      break;
+    case ActionType.CREATE_COMMENT:
+      api.createComment({
+        id: updatedFilm.id,
+        comment: updatedFilm.comment,
+      })
+        .then(() => api.getFilms())
+        .then((movies) => {
+          pageController.show(movies);
+          callBackFunction();
+        });
+      break;
+    case ActionType.DELETE_COMMENT:
+      api.deleteComment({
+        commentId: updatedFilm.id,
+      })
+        .then(() => api.getFilms())
+        .then((movies) => {
+          pageController.show(movies);
+          callBackFunction();
+        });
+      break;
+    case ActionType.UPDATE_RATING:
+      api.updateFilm({
+        id: updatedFilm.id,
+        film: updatedFilm.toRAW(),
+      })
+        .then(() => api.getFilms())
+        .then((movies) => {
+          menuController.show(movies);
+          pageController.show(movies);
+          searchController.show(movies);
+        });
+      break;
+    default:
+      throw new Error(`Error onDataChenge`);
   }
 };
 
-const api = new API({endPoint: END_POINT, authorization: AUTHORIZATION});
-const filmsControllerInstance = new PageController(mainElement, onDataChange);
+
+// Контроллеры
+const chartController = new StatisticController(mainElement);
+const pageController = new PageController(mainElement, onDataChange);
+const searchController = new SearchController(mainElement, searchComponent, onDataChange, onSearchCloseClick);
+const menuController = new MenuController(mainElement, pageController, searchController, chartController);
 
 
-api.getMovies().then((movies) => {
-  const statisticsComponent = new Statistics(movies);
-
-  filmsControllerInstance.init(movies);
-
-  mainElement.appendChild(statisticsComponent.getElement());
-  const statisticsLink = mainElement.querySelector(`.main-navigation__item--additional`);
-  const showAllLink = mainElement.querySelector(`.main-navigation__item`);
+// отрисовка статики Поиска и Загрзки до ответа сервера
+renderElement(headerElement, searchComponent.getElement());
+renderElement(mainElement, loadingComponent.getElement());
 
 
-  const showStatistics = (evt) => {
-    evt.preventDefault();
-
-    statisticsComponent.show();
-    filmsControllerInstance.hide();
-
-    const chart = new StatisticController(movies);
-    chart._renderChart();
-  };
-
-  const showMovies = (evt) => {
-    evt.preventDefault();
-
-    statisticsComponent.hide();
-    filmsControllerInstance.show();
-  };
-
-  statisticsLink.addEventListener(`click`, showStatistics);
-  showAllLink.addEventListener(`click`, showMovies);
+// Создание профиля пользователя
+api.getFilms().then((films) => {
+  const profileComponent = new Profile(films);
+  renderElement(headerElement, profileComponent.getElement());
 });
+
+
+// Скрыть елементы вне дефю состояния
+const hideMainPage = () => {
+  menuController.hide();
+  pageController.hide();
+  chartController.hide();
+  setSearchState(true);
+};
+
+
+// Вернуть мейн к дефолту
+const initMainPage = () => {
+  searchController.hide();
+  chartController.hide();
+  pageController._init();
+  setSearchState(false);
+  onDataChange(ActionType.CREATE);
+};
+
+
+searchComponent.getElement().querySelector(`.search__field`).addEventListener(`keyup`, (evt) => {
+  const eventLength = evt.target.value.length;
+
+  if (eventLength >= MIN_SEARCH_PHRASE) {
+    hideMainPage();
+    api.getFilms().then((movies) => searchController.show(movies));
+  } else if (eventLength === 0) {
+    initMainPage();
+  }
+});
+
+
+initMainPage();
