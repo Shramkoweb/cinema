c62605611989f5dde30ebca7509aef89a2452550
Serhii.Shramko <shramko.web@yahoo.com>
2019-10-04 21:10:47 +0300

    feat: update SortController

--- src/controllers/sort-controller.js
+++ src/controllers/sort-controller.js
@@ -1,11 +1,11 @@
 import Sort from "../components/sort";
-import {renderElement, sortFilms} from "../utils";
 import {Position} from "../constants";
+import {renderElement, sortFilms} from "../utils";
 
 class SortController {
-  constructor(container, renderFilmsList) {
+  constructor(container, renderFilms) {
     this._container = container;
-    this._renderFilmsList = renderFilmsList;
+    this._renderFilms = renderFilms;
     this._sortComponent = new Sort();
   }
 
@@ -17,7 +17,6 @@ class SortController {
     this._sortComponent.getElement().classList.add(`visually-hidden`);
   }
 
-
   assignFilms(films) {
     this._films = films;
     this._sorteredFilms = films.slice();
@@ -28,14 +27,14 @@ class SortController {
     const onSortLinkClick = (evt) => {
       evt.preventDefault();
 
-      // Убираю активный класс
-      this._sortComponent.getElement()
-        .querySelector(`.sort__button--active`).classList
-        .remove(`sort__button--active`);
-
-      evt.target.classList.add(`sort__button--active`);
-
       if (evt.target.tagName === `A`) {
+
+        // Убираю активный класс
+        this._sortComponent.getElement()
+          .querySelector(`.sort__button--active`).classList
+          .remove(`sort__button--active`);
+
+
         // Активный класс на целевой елемент
         evt.target.classList.add(`sort__button--active`);
 
@@ -46,13 +45,12 @@ class SortController {
 
         // отрисовка фильмов
         this._sorteredFilms = sortFilms(this._films, sortType);
-        this._renderFilmsList(this._sorteredFilms);
+        this._renderFilms(this._sorteredFilms);
       }
     };
 
     // Вешаю обработчики клика на кнопки сортировки
     this._sortComponent.getElement().addEventListener(`click`, (evt) => onSortLinkClick(evt));
-
     renderElement(this._container, this._sortComponent.getElement(), Position.AFTERBEGIN);
   }
 }
