a45edcf8cefcd1b91d922e870e38a9ca9ef8e97c
Serhii.Shramko <shramko.web@yahoo.com>
2019-10-04 21:07:13 +0300

    feat: update MenuController

--- src/controllers/menu-controller.js
+++ src/controllers/menu-controller.js
@@ -1,26 +1,23 @@
 import Menu from '../components/menu';
-import {Position, PageFilterTitle} from '../constants';
+import {Position, PageFilterTitle, MenuFilter} from '../constants';
 import {renderElement, unrenderElement, getFiltersAmount} from '../utils';
 
 class MenuController {
-  constructor(container, pageController) {
+  constructor(container, pageController, searchController, chartController) {
+    this._chartController = chartController;
     this._container = container;
-    this._pageController = pageController;
-
+    this._films = [];
+    this._filterAmount = {};
     this._isSearch = false;
     this._menu = null;
-    this._filterAmount = {};
-    this._films = [];
+    this._pageController = pageController;
+    this._searchController = searchController;
   }
 
   show(films) {
     if ((!this._isSearch) && (films !== this._films)) {
       this._setFilms(films);
     }
-  }
-
-  setSearch(isSearch) {
-    this._isSearch = isSearch;
   }
 
   hide() {
@@ -30,59 +27,63 @@ class MenuController {
 
   _setFilms(films) {
     this._films = films.slice();
-
     this._renderMenu(this._films);
   }
 
   _renderMenu(films) {
     this._filterAmount = getFiltersAmount(films);
-
     if (this._menu !== null) {
       this.hide();
     }
-
     this._menu = new Menu(this._filterAmount);
-
     this._init();
   }
 
+  _getFilteredFilms(filterType) {
+    const filteredFilmCards = this._films.slice().filter((elem) => elem[filterType]);
+    this._pageController.show(filteredFilmCards);
+  }
+
+  setSearch(isSearch) {
+    this._isSearch = isSearch;
+  }
+
   _init() {
-    const onMenuElemClick = (evt) => {
+    const onMenuElementClick = (evt) => {
       evt.preventDefault();
-      if (!evt.target.classList.contains(`main-navigation__item`)) {
-        return;
-      }
-      this._menu.getElement()
-        .querySelector(`.main-navigation__item--active`).classList
-        .remove(`main-navigation__item--active`);
 
-      // Активный класс на целевой елемент
-      evt.target.classList.add(`main-navigation__item--active`);
+      if (evt.target.tagName === `A`) {
+        this._menu.getElement()
+          .querySelector(`.main-navigation__item--active`).classList
+          .remove(`main-navigation__item--active`);
 
+        // Активный класс на целевой елемент
+        evt.target.classList.add(`main-navigation__item--active`);
 
-      switch (evt.target.hash.slice(1)) {
-        case PageFilterTitle.ALL:
-          this._pageController.renderFilmList(this._films);
-          break;
-        case PageFilterTitle.WATCHLIST:
-          this._pageController.setFilterState(PageFilterTitle.WATCHLIST);
-          break;
-        case PageFilterTitle.HISTORY:
-          this._pageController.setFilterState(PageFilterTitle.HISTORY);
-          break;
-        case PageFilterTitle.FAVORITES:
-          this._pageController.setFilterState(PageFilterTitle.FAVORITES);
-          break;
-        case PageFilterTitle.STATS:
-          console.log('STATS');
-          break;
-        default:
-          break;
+        switch (evt.target.hash.slice(1)) {
+          case PageFilterTitle.ALL:
+            this._pageController.show(this._films);
+            break;
+          case PageFilterTitle.WATCHLIST:
+            this._getFilteredFilms(MenuFilter.Watchlist);
+            break;
+          case PageFilterTitle.HISTORY:
+            this._getFilteredFilms(MenuFilter.History);
+            break;
+          case PageFilterTitle.FAVORITES:
+            this._getFilteredFilms(MenuFilter.Favorites);
+            break;
+          case PageFilterTitle.STATS:
+            this._pageController.hide();
+            this._searchController.hide();
+            this._chartController.show(this._films);
+            break;
+          default:
+            break;
+        }
       }
     };
-
-    this._menu.getElement().addEventListener(`click`, onMenuElemClick);
-
+    this._menu.getElement().addEventListener(`click`, onMenuElementClick);
     renderElement(this._container, this._menu.getElement(), Position.AFTERBEGIN);
   }
 }
